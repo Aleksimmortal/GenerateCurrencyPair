8ffe24ec69be3b607cf275097794068073b8ee71
Aleksimmortal <aleksimmortal@mail.ru>
2024-01-20 13:48:52 +0300

    Раскидал по классам

--- Program.cs
+++ Program.cs
@@ -1,74 +1,20 @@
-﻿using System;
+﻿using GenerateCurrencyPair;
+using System;
 using System.Text.Json;
 
-namespace CurrencyPair
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        private const string CurrencyPairNote = "C:\\Users\\pollo\\OneDrive\\Desktop\\проект\\Currency.json";
-        public class SaveCurrencyPair
-        {
-            public DateTime Date { get; set; }
-            public string? CurrencyPair { get; set; }
-            public double Value { get; set; }
+        // устанавливаем метод обратного вызова
+        var obj = new CountClass();
+        TimerCallback tm = new TimerCallback(obj.Circle);
+        // создание таймера, 3000 это периодичность вывода информации в миллисекундах, т.е. 3 секунды
+        Timer timer = new Timer(tm, null, 0, 3000);
+        Console.ReadLine();
 
 
-
-            public void Create(SaveCurrencyPair notesPair)
-            {
-                var currencyNotes = new List<SaveCurrencyPair>();
-                if (File.Exists(CurrencyPairNote))
-                {
-                    using (FileStream fs = new FileStream(CurrencyPairNote, FileMode.OpenOrCreate))
-                    {
-                        currencyNotes = JsonSerializer.Deserialize<List<SaveCurrencyPair>>(fs);
-                    }
-                }
-                currencyNotes.Add(notesPair);
-                Write(currencyNotes);
-
-            }
-
-            public void Write(List<SaveCurrencyPair> notesPair)
-            {
-                using (FileStream fs = new FileStream(CurrencyPairNote, FileMode.OpenOrCreate))
-                {
-                    JsonSerializer.Serialize<List<SaveCurrencyPair>>(fs, notesPair);
-                }
-            }
-        
-        public void PrintCurrencyPair()
-            {
-                Console.WriteLine($"Дата: {Date} Валютная пара: {CurrencyPair} Отношение валютных пар: {Value}");
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            // устанавливаем метод обратного вызова
-            TimerCallback tm = new TimerCallback(Count);
-            // создание таймера, 3000 это периодичность вывода информации в миллисекундах, т.е. 3 секунды
-            Timer timer = new Timer(tm, null, 0, 3000);
-            Console.ReadLine();
-        }
-
-        public static void Count(object obj)
-        {
-            Random random = new Random();
-           
-                var saveCP = new SaveCurrencyPair
-                {
-                    Date = DateTime.Now,
-                    CurrencyPair = "Currency pair RUR/USD",
-                    Value = Math.Round(((random.NextDouble()* 0.019)+ 0.011),3)
-                };
-                saveCP.PrintCurrencyPair();
-                saveCP.Create(saveCP);
-            }
-        }
-
-        
-        
-    
+    }
 }
+   
 
